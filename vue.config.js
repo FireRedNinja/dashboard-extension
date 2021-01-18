751b4b632c7b13c7e8a46c85892fb5174469fa45
Noel Rajan <noel.rajan@skyscanner.net>
2021-01-18 16:12:31 +0000

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,19 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
 
 module.exports = {
   outputDir: "dist",
   pages: {
     index: {
       // entry for the page
-      entry: 'src/main.js',
+      entry: "src/main.js",
       // the source template
-      template: 'public/index.html',
+      template: "public/index.html",
       // output as dist/index.html
-      filename: 'index.html',
+      filename: "index.html",
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Dashboard',
+      title: "Dashboard",
     },
   },
   configureWebpack: {
@@ -21,9 +22,18 @@ module.exports = {
         patterns: [
           {
             from: "./manifest.json",
-            to: "./manifest.json"
+            to: "./manifest.json",
           },
         ],
+      }),
+      new ZipPlugin({
+        // OPTIONAL: defaults to the Webpack output path (above)
+        // can be relative (to Webpack output path) or absolute
+        path: "../",
+
+        // OPTIONAL: defaults to the Webpack output filename (above) or,
+        // if not present, the basename of the path
+        filename: "firefox.zip",
       }),
     ],
   },
